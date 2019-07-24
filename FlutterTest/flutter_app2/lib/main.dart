6b71236d164314a1b7a99ae66d56223f0c90ce1b
wbx <18867521887@139.com>
2019-07-24 20:32:21 +0800

    fix #Image component

--- FlutterTest/flutter_app2/lib/main.dart
+++ FlutterTest/flutter_app2/lib/main.dart
@@ -36,4 +36,16 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class ImageDemo extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Center(
+      child: new Image.network('http://image.baidu.com/search/detail?ct=503316480&z=undefined&tn=baiduimagedetail&ipn=d&word=%E5%9B%BE%E7%89%87&step_word=&ie=utf-8&in=&cl=2&lm=-1&st=undefined&hd=undefined&latest=undefined&copyright=undefined&cs=180868167,273146879&os=17615934,454074307&simid=3310604703,440404647&pn=0&rn=1&di=189640&ln=742&fr=&fmq=1563971062124_R&fm=&ic=undefined&s=undefined&se=&sme=&tab=0&width=undefined&height=undefined&face=undefined&is=0,0&istype=0&ist=&jit=&bdtype=0&spn=0&pi=0&gsm=0&objurl=http%3A%2F%2Fimg.redocn.com%2Fsheji%2F20141219%2Fzhongguofengdaodeliyizhanbanzhijing_3744115.jpg&rpstart=0&rpnum=0&adpicid=0&force=undefined'
+      , fit: BoxFit.fitWidth,
+      ),
+    );
+  }
+}
+
 

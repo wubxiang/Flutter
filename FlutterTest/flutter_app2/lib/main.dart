b7bfe4babae242d125917fdf7b395eaa8ad9ecba
wbx <18867521887@139.com>
2019-09-09 19:18:15 +0800

    fix #new project Signed-off-by: wbx <18867521887@139.com>

--- FlutterTest/flutter_app2/lib/main.dart
+++ FlutterTest/flutter_app2/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:english_words/english_words.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
     @override
   Widget build(BuildContext context) {
     // TODO: implement build

b7a8556bc66216171a7739fed71a27fdcc6ad1ab
wbx <1439262759@qq.com>
2019-12-05 16:13:44 +0800

    Signed-off-by: wbx <1439262759@qq.com>

--- flutter_app3/lib/main.dart
+++ flutter_app3/lib/main.dart
@@ -6,10 +6,15 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  // 重写build方法来构建一个组件，build即为创建一个widget的意思，返回值也是一个widget
   @override
   Widget build(BuildContext context) {
+    //MaterialApp 应用组件，代表使用Material Design风格的应用
     return new MaterialApp(
+      //应用程序的标题，Android 出现在任务管理器的程序快照之上，iOS 出现在程序切换管理器中
       title: '导航页面示例',
+      //home 这是一个widget对象，用来定义当前应用打开时所显示的界面
+      //Scaffold 脚手架组件，实现了基本的Material Design 布局
       theme: ThemeData(
         //主题颜色
         primarySwatch: Colors.blue,
